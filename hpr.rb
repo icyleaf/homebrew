e301dd02ab40291993ce7f2a19c5ae0f981d6cb3
icyleaf <icyleaf.cn@gmail.com>
2018-04-26 16:21:43 +0800

    add sha256 to hpr

--- hpr.rb
+++ hpr.rb
@@ -5,14 +5,14 @@ class Hpr < Formula
   desc 'Mirror git repositories to self-host gitlab services.'
   homepage 'https://github.com/icyleaf/hpr'
   url "https://github.com/icyleaf/hpr/archive/v#{VERSION}.tar.gz"
+  sha256 '61947a9f3ed2f33a9421f4c514775d0db3acead144616e318af7cb20bd797d60'
 
   depends_on 'crystal-lang' => :build
   depends_on 'redis'
 
   def install
-    system "mkdir lib"
     system "shards build --release --no-debug"
-    bin.install "hpr"
+    bin.install "bin/hpr"
   end
 
   test do
